6004117da787d039b67a4afcc6e61052c1c6f5ad
skipmodea1 <skipmodea1@outlook.com>
2016-02-16 05:40:15 +0100

    Revert "v1.1.2 (2016-02-13)"
    This reverts commit 796575fc5ebcbe1d431467d481bded40788efdde.

--- resources/lib/botchamania_utils.py
+++ resources/lib/botchamania_utils.py
@@ -1,90 +1,91 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-import StringIO
-import gzip
+import zlib
 import httplib
 import urllib
 import urllib2
+import gzip
+import StringIO
+import urlparse
 
-
 #
 #
 #
-class HTTPCommunicator:
+class HTTPCommunicator :
     #
     # POST
     #
-    def __init__(self):
-        pass
-
-    @staticmethod
-    def post(host, url, params):
-        parameters = urllib.urlencode(params)
-        headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain",
-                   "Accept-Encoding": "gzip", "Cookie": "TnetID=1lmx3gb83nsK7j82d8f_OJp0uH7sjsE4"}
-        connection = httplib.HTTPConnection("%s:80" % host)
-
-        connection.request("POST", url, parameters, headers)
+    def post( self, host, url, params ):
+        parameters  = urllib.urlencode( params )
+        headers     = { "Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain", "Accept-Encoding" : "gzip" }
+        connection  = httplib.HTTPConnection("%s:80" % host)
+        
+        connection.request( "POST", url, parameters, headers )
         response = connection.getresponse()
-
+        
         # Compressed (gzip) response...
-        if response.getheader("content-encoding") == "gzip":
-            html_gzipped_data = response.read()
-            string_io = StringIO.StringIO(html_gzipped_data)
-            gzipper = gzip.GzipFile(fileobj=string_io)
-            html_data = gzipper.read()
+        if response.getheader( "content-encoding" ) == "gzip" :
+            htmlGzippedData = response.read()
+            stringIO       = StringIO.StringIO( htmlGzippedData )
+            gzipper        = gzip.GzipFile( fileobj = stringIO )
+            htmlData       = gzipper.read()
         # Plain text response...
-        else:
-            html_data = response.read()
+        else :
+            htmlData = response.read()
 
         # Cleanup
         connection.close()
 
         # Return value
-        return html_data
+        return htmlData
 
     #
     # GET
     #
-    @staticmethod
-    def get(url):
-        user_agent = "Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.72 Safari/537.36"
+    def get( self, url ):
+        user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:29.0) Gecko/20100101 Firefox/29.0'
         values = {}
-        headers = {'User-Agent': user_agent,
-                   'Accept-Encoding': 'gzip',
-                   "Cookie": 'TnetID=1lmx3gb83nsK7j82d8f_OJp0uH7sjsE4'}
+        headers = { 'User-Agent'      : user_agent , 
+                    'Accept-Encoding' : 'gzip'     }
         data = urllib.urlencode(values)
         req = urllib2.Request(url, data, headers)
-        f = urllib2.urlopen(req)
-
+        #sometimes the site gives a http error 408 (timeout), hopefully this fixes it...
+        try:
+            f = urllib2.urlopen(req)
+        except:
+            f = urllib2.urlopen(req)
+            
         # Compressed (gzip) response
-        if f.headers.get("content-encoding") == "gzip":
-            html_gzipped_data = f.read()
-            string_io = StringIO.StringIO(html_gzipped_data)
-            gzipper = gzip.GzipFile(fileobj=string_io)
-            html_data = gzipper.read()
-
+        if f.headers.get( "content-encoding" ) == "gzip" :
+            htmlGzippedData = f.read()
+            stringIO        = StringIO.StringIO( htmlGzippedData )
+            gzipper         = gzip.GzipFile( fileobj = stringIO )
+            htmlData        = gzipper.read()
+            
+            # Debug
+            # print "[HTTP Communicator] GET %s" % url
+            # print "[HTTP Communicator] Result size : compressed [%u], decompressed [%u]" % ( len( htmlGzippedData ), len ( htmlData ) )
+            
         # Plain text response
-        else:
-            html_data = f.read()
-
+        else :
+            htmlData = f.read()
+        
         # Cleanup
         f.close()
 
         # Return value
-        return html_data
+        return htmlData
 
     #
     # Check if URL exists
     #
-    @staticmethod
-    def exists(url):
-        try:
-            request = urllib2.Request(url)
-            request.get_method = lambda: 'HEAD'
-            response = urllib2.urlopen(request)
+    def exists( self, url ):
+        try :
+            request            = urllib2.Request( url )
+            request.get_method = lambda : 'HEAD'
+            response           = urllib2.urlopen( request )
             response.close()
             return True
-        except:
+        except :
             return False
